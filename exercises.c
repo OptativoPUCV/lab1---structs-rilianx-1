0b44910212b16c7eebdb8f01812f6c37722455da
Your Name <you@example.com>
2025-03-06 11:56:08 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -11,7 +11,7 @@ y su tamaño, y devuelva el valor más grande del arreglo.
 */
 int findMax(int arr[], int size) { 
   int max = 0;
-  for (int i; i<size; i++)
+  for (int i=0; i<size; i++)
      if (arr[i]>max) max=arr[i];
   
   return max; 
